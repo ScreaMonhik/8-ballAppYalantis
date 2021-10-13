b4b84c52f4257cc13e875e71d98c53fe2b2db669
Sunko Dmytro <virag5002@gmail.com>
2021-10-13 12:52:01 +0300

    Initial Commit

---
+++ 8-ballAppYalantis/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  8-ballAppYalantis
+//
+//  Created by Dima Sunko on 13.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
